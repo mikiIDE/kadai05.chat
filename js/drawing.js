e7aa519ddf0e685ab0fe846b2e935168f4f4cfae
mikiIDE <kinoko.miki@gmail.com>
2024-10-28 16:58:33 +0900

    Update drawing.js
    不要な記述が残っていたため削除しました

--- js/drawing.js
+++ js/drawing.js
@@ -1,6 +1,12 @@
 // お絵描きのためのコード
-const canvas = document.querySelector('#drawing-area');
-const ctx = canvas.getContext('2d');
+
+//ctx.lineWidth = 10; //線の太さ
+//ctx.lineCap = 'square'; //線の先が四角（roundもあり）
+//ctx.beginPath(); //始点
+//ctx.closePath(); //終点
+
+const can = $('#drawing-area')[0];
+const ctx = can.getContext('2d');
 const clearBtn = $('#clear-button');
 
 let x;
@@ -10,7 +16,7 @@ let mousePressed = false;
 // 描画を開始する
 function startDrawing(e) {
     mousePressed = true;
-    x = e.offsetX;
+    x = e.offsetX; //offsetでキャンバス内の位置を捕捉
     y = e.offsetY;
 }
 
@@ -19,58 +25,14 @@ function draw(e) {
     if (!mousePressed) return;
     const x2 = e.offsetX;
     const y2 = e.offsetY;
-    ctx.beginPath();
-    ctx.moveTo(x, y);
+    ctx.beginPath(); //始点
+    ctx.moveTo(x, y); //(x,y)の座標に動く
     ctx.lineTo(x2, y2);
-    ctx.stroke();
-    x = x2;
+    ctx.stroke(); //描画ストローク
+    x = x2; //座標の位置を更新して繰り返していく
     y = y2;
 }
 
 $('#drawing-area').on('mousedown', startDrawing);
 $('#drawing-area').on('mousemove', draw);
 $(window).on('mouseup', () => mousePressed = false);
-
-
-// //お絵描きのためのコード
-// const canvas = document.querySelector('#drawing-area');
-// const ctx = canvas.getContext('2d');
-// const clearBtn = document.querySelector('#clear-button');
-
-// let x;
-// let y;
-// let mousePressed = false;
-
-// //描画を開始する
-// function startDrawing(e) {
-//     mousePressed = true;
-//     x = e.offsetX;
-//     y = e.offsetY;
-// }
-
-// //線を描画する
-// function draw(e) {
-//     if (!mousePressed) return;
-//     x2 = e.offsetX;
-//     y2 = e.offsetY;
-//     ctx.beginPath();
-//     ctx.moveTo(x, y);
-//     ctx.lineTo(x2, y2);
-//     ctx.stroke();
-//     x = x2;
-//     y = y2;
-// }
-
-// //マウス押下で描画開始
-// canvas.addEventListener('mousedown', startDrawing);
-
-// //マウスを動かして描画
-// canvas.addEventListener('mousemove', draw);
-
-// //マウスボタンを離して描画終了
-// window.addEventListener('mouseup', () => mousePressed = false);
-
-// ///消去ボタンクリックで全消去
-// clearBtn.addEventListener('click', () => {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-// });
