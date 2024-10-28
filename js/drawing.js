c54fbe2052b743bf522057a303c7f59a3d6e8233
mikiIDE <kinoko.miki@gmail.com>
2024-10-28 23:33:46 +0900

    Update drawing.js
    線の太さや色の変更ができるようにしました

--- js/drawing.js
+++ js/drawing.js
@@ -4,10 +4,13 @@
 //ctx.lineCap = 'square'; //線の先が四角（roundもあり）
 //ctx.beginPath(); //始点
 //ctx.closePath(); //終点
+//strokeStyle //線の色
+//fillStyle //塗りつぶしの色
 
 const can = $('#drawing-area')[0];
 const ctx = can.getContext('2d');
 const clearBtn = $('#clear-button');
+ctx.lineCap = 'round';
 
 let x;
 let y;
@@ -36,3 +39,32 @@ function draw(e) {
 $('#drawing-area').on('mousedown', startDrawing);
 $('#drawing-area').on('mousemove', draw);
 $(window).on('mouseup', () => mousePressed = false);
+
+$("#penS").on("click", function () {
+    ctx.lineWidth = 1; 
+});
+$("#penM").on("click", function () {
+    ctx.lineWidth = 5; 
+});
+$("#penL").on("click", function () {
+    ctx.lineWidth = 10; 
+});
+
+$("#black").on("click",function(){
+    ctx.strokeStyle = "black";
+});
+$("#white").on("click",function(){
+    ctx.strokeStyle = "white";
+});
+$("#red").on("click",function(){
+    ctx.strokeStyle = "red";
+});
+$("#blue").on("click",function(){
+    ctx.strokeStyle = "blue";
+});
+$("#yellow").on("click",function(){
+    ctx.strokeStyle = "yellow";
+});
+$("#green").on("click",function(){
+    ctx.strokeStyle = "green";
+});
